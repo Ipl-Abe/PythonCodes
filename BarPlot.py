738c772a42cdb61210f97cb9321d34cfcbaa0d65
Ipl-Abe <crowsky7275@gmail.com>
2019-11-19 02:10:31 +0900

    modify BarPlot.py

--- BarPlot.py
+++ BarPlot.py
@@ -1,6 +1,5 @@
 # author : Fumiaki Abe
 
-# TODO : sort big number
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
@@ -11,12 +10,15 @@ args = sys.argv
 
 leftList = []
 heightList = []
+leftList2 = []
+heightList2 = []
 List = []
+List2 = []
 
-if len(sys.argv) < 2:
+if len(sys.argv) == 1:
     print("add file name !")
     exit
-else:
+elif len(sys.argv) == 2:
     filename = '/home/rel/Desktop/ContactLink_ex/' + args[1]
     with open(filename) as f:
         reader = csv.reader(f)
@@ -26,9 +28,38 @@ else:
     sortsecond = lambda val: val[1]
     List.sort(key=sortsecond)
     for row in List:
+        
         leftList.append(row[0])
         heightList.append(int(row[1]))
     plt.barh(leftList, heightList )
     plt.show()
 
+elif len(sys.argv) == 3:
+    filename1 = '/home/rel/Desktop/ContactLink_ex/' + args[1]
+    filename2 = '/home/rel/Desktop/ContactLink_ex/' + args[2]
+    # file1
+    with open(filename1) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            List.append([row[0],int(row[1])])
+    for row in List:
+        leftList.append(row[0])
+        heightList.append(int(row[1]))
+    # file2
+    with open(filename2) as f2:
+        reader2 = csv.reader(f2)
+        for row in reader2:
+            List2.append([row[0],int(row[1])])
+    for row in List2:
+        leftList2.append(row[0])
+        heightList2.append(int(row[1]))
 
+    left = np.arange(len(heightList))
+    height = 0.3
+
+    plt.barh(left, heightList, height=height, align='center')
+    plt.barh(left+height, heightList2, height=height, align='center')
+    plt.yticks(left + height/2, leftList)
+
+
+    plt.show()
